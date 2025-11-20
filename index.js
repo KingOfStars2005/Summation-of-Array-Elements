d95a487e799d3b0a36bc91020ba8cba06c3aab63
Anurag Shre <aquaflames2005@gmail.com>
2025-11-20 22:52:11 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const arr = [1,2,3,4,5,10]
+let arr = [1,2,3,4,5,10]
+let sum = 0
+alert(`Welcome \nthe array is: ${arr} \nand you will see the sum of all the elements`)
+
+for(let i = 0; i<arr.length;i++){
+    sum += arr[i]
+}
+alert(sum)
+console.log(sum);
